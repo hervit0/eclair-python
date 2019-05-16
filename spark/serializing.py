d2e125a75b6d6785c32e69b1cc082707548b38df
Hervé Ah-Leung <herve.ahleung@gmail.com>
2019-05-16 10:52:34 +0100

    Spark > Serializing

---
+++ spark/serializing.py
@@ -0,0 +1,8 @@
+from pyspark.context import SparkContext
+from pyspark.serializers import MarshalSerializer
+
+sc = SparkContext("local", "serialization app", serializer=MarshalSerializer())
+sc.setLogLevel("ERROR")
+
+print(sc.parallelize(list(range(1000))).map(lambda x: 2 * x).take(10))
+sc.stop()
